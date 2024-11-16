cc8918462ef21a65aafa185d192fe978352d15ed
Daniele <ristanidaniele@yahoo.it>
2024-11-17 00:32:52 +0100

    addRegister

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+  
+  
+  registerForm!:FormGroup
+  
+  matchPassword(controls: AbstractControl): ValidationErrors | null {
+    const password = controls.get('password')?.value;
+    const confirmPassword = controls.get('confirmPassword')?.value;
+    if (password !== confirmPassword) {
+      return {mismatch: true};
+    }
+    return null;
+  }
+  ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      title: new FormControl(null),
+      firstName: new FormControl(null,[Validators.required,Validators.minLength(3),Validators.maxLength(10)]),
+      middleName: new FormControl(null),
+      lastName: new FormControl(null,[Validators.required,Validators.minLength(3),Validators.maxLength(10)]),
+      suffix: new FormControl(null),
+      email: new FormControl(null,[Validators.required,Validators.email]),
+      phone: new FormControl(null,[Validators.required,Validators.minLength(10),Validators.maxLength(10)]),
+      password: new FormControl(null,[Validators.required,Validators.minLength(8)]),
+      confirmPassword: new FormControl(null,[Validators.required,this.matchPassword.bind(this)]),
 
+    })
+  }
 }
