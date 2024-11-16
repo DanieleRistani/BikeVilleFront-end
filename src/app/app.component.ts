9d81d8649dd6467f12650dfe701382123f37e0b8
Daniele <ristanidaniele@yahoo.it>
2024-11-16 21:30:10 +0100

    darkMode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./static/nav-bar/nav-bar.component";
 import { FooterComponent } from "./static/footer/footer.component";
 import { HeaderComponent } from "./shared/header/header.component";
-import { CommonModule } from '@angular/common';
-
-
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, FooterComponent, HeaderComponent, CommonModule],
+  imports: [RouterOutlet, NavBarComponent, FooterComponent, HeaderComponent,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/static/nav-bar/nav-bar.component.ts
+++ src/app/static/nav-bar/nav-bar.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
+import { FormsModule } from '@angular/forms'
+import { NgModel } from '@angular/forms';
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,FormsModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
+  ngOnInit(): void {
+    if( window.localStorage.getItem('isNight')){
+      
+    }else{
+      window.localStorage.setItem('isNight', 'false')
+    }
+    console.log(window.localStorage.getItem('isNight'));
+    console.log(this.isNight);
+    
+  }
+ 
+  isNight: boolean = window.localStorage.getItem('isNight') == 'true' ? true : false
+  turnOnNightMode() {
+    if (this.isNight == false) {
+      window.localStorage.setItem('isNight', 'true')
+      document.body.classList.add('night-mode');
+      document.body.classList.remove('light-mode');
+    }
+    else {
+      window.localStorage.setItem('isNight', 'false')
+      document.body.classList.remove('night-mode');
+      document.body.classList.add('light-mode');
+    }
+  }
+
 
 }
