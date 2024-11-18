eca7fa961b44f6ca1068a2708cc6cdc4c78ff781
Dan <ristanidaniele@yahoo.it>
2024-11-18 13:46:27 +0100

    aggiunta registrazione

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,35 +1,38 @@
 import { Component, Input, OnInit } from '@angular/core';
-import {FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { NgClass } from '@angular/common';
-import { NgIf,NgFor } from '@angular/common';
+import { NgIf, NgFor } from '@angular/common';
 import { FormBuilder } from '@angular/forms';
+import { RegisterService } from '../../service/auth/register.service';
+import { first } from 'rxjs';
+import { Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule,NgClass,NgFor,NgIf],
+  imports: [ReactiveFormsModule, NgClass, NgFor, NgIf],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent implements OnInit{
-  
-  constructor(private formBuilder: FormBuilder) { }
-  registerForm!:FormGroup
-  
+export class RegisterComponent implements OnInit {
+
+  constructor(private formBuilder: FormBuilder, private registerService: RegisterService) { }
+  registerForm!: FormGroup
+
 
   ngOnInit(): void {
     this.registerForm = this.formBuilder.group({
       title: new FormControl(null),
-      firstName: new FormControl(null,[Validators.required,Validators.minLength(3),Validators.maxLength(10)]),
+      firstName: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(10)]),
       middleName: new FormControl(null),
-      lastName: new FormControl(null,[Validators.required,Validators.minLength(3),Validators.maxLength(10)]),
+      lastName: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(10)]),
       suffix: new FormControl(null),
-      email: new FormControl(null,[Validators.required,Validators.email]),
-      phone: new FormControl(null,[Validators.required,Validators.minLength(10),Validators.maxLength(10)]),
-      password: new FormControl(null,[Validators.required,Validators.minLength(8)]),
-      confirmedPassword: new FormControl(null,[Validators.required]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      phone: new FormControl(null, [Validators.required, Validators.minLength(10), Validators.maxLength(10)]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+      confirmedPassword: new FormControl(null, [Validators.required]),
 
-    },{validators: this.passwordsMatch})
-    
+    }, { validators: this.passwordsMatch })
+
   }
 
   passwordsMatch(controls: FormGroup): ValidationErrors | null {
@@ -40,5 +43,28 @@ export class RegisterComponent implements OnInit{
       return { mismatch: true };
     }
     return null;
+  }
+
+  addUser() {
+
+    if (this.registerForm.valid) {
+
+      this.registerService.register({
+        Title: this.registerForm.value.title,
+        FirstName: this.registerForm.value.firstName,
+        MiddleName: this.registerForm.value.middleName,
+        LastName: this.registerForm.value.lastName,
+        Suffix: this.registerForm.value.suffix,
+        EmailAddress: this.registerForm.value.email,
+        Phone: this.registerService.formattaNumero(this.registerForm.value.phone.toString()),
+        Password: this.registerForm.value.password,
+        Role: "USER",
+      }).subscribe((data: any) => {
+        console.log(data);
+      })
+    } else {
+      console.log("Invalid Form", this.registerForm.value);
+      
+    }
   }
 }
--- src/app/service/auth/register.service.ts
+++ src/app/service/auth/register.service.ts
@@ -8,6 +8,20 @@ export class RegisterService {
   constructor(private http :HttpClient) { }
 
   register(data:any){
+    console.log(data);
+    
     return this.http.post('https://localhost:7167/Users/Add',data);
   }
+
+  formattaNumero(numero: string ): string {
+    
+    const numeroStr = numero.toString();
+
+   
+    if (numeroStr.length !== 10 || !/^\d{10}$/.test(numeroStr)) {
+        return "Errore: il numero deve avere 10 cifre.";
+    }
+    const formato = `${numeroStr.slice(0, 3)}-${numeroStr.slice(3, 6)}-${numeroStr.slice(6)}`;
+    return formato;
+}
 }
